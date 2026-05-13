dc303268bb9451f80c2ca91c6f62823f943ab9d7
reynaldramirez@hotmail.com <reynaldramirez@hotmail.com>
2026-05-13 13:55:36 -0400

    [U] I twist the Heavens with his voice - Jesus of Nazareth

---
+++ models/MainModel.php
@@ -0,0 +1,18 @@
+<?php
+require_once("../j_videos/private/conexion.php");
+
+class MainModel {
+
+    protected $Conexion = ""; // Conexion string that completes the process of the conexion.
+
+
+    function get_connected($conn) {
+        $conexion_custom_string = "";
+
+        return $conexion_custom_string;
+    }
+
+}
+
+
+?>
